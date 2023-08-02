c241fe7d5b77a9f7204786cf5b64b1c29dee2f8f
jl666 <jl3362518@163.com>
2023-08-02 22:38:39 +0800

    重构了项目代码

--- verify.py
+++ verify.py
@@ -1,5 +1,6 @@
 import pyotp
 import qrcode
+import tkinter as tk
 from PIL import ImageTk
 
 
@@ -20,7 +21,30 @@ def init():
         f.write(sec)
     totp = pyotp.TOTP(sec)
     uri = totp.provisioning_uri(name='admin', issuer_name='demo')
-    img = qrcode.make(uri).resize((200,  200))
-    print(img)
+    img = qrcode.make(uri).resize((200, 200))
+    print(totp.now())
     tk_img = ImageTk.PhotoImage(img)
     return tk_img
+
+
+def verify_gui():
+    root = tk.Tk()
+    root.geometry("300x100")
+    root.protocol("WM_DELETE_WINDOW", exit)
+
+    f1 = tk.Frame(root, pady=25)
+    f1.pack()
+
+    l1 = tk.Label(f1, text="请输入验证码：")
+    l1.pack(side="left")
+
+    e1 = tk.Entry(f1)
+    e1.pack()
+    b1 = tk.Button(root, text='验证', command=lambda: gui_exit(e1.get(), root))
+    b1.pack()
+    root.mainloop()
+
+
+def gui_exit(user, window):
+    if verify(user):
+        window.destroy()
