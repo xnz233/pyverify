32a17ef4defbe77a418a06dac1faffe3bf6faaf8
jl666 <jl3362518@163.com>
2023-08-03 13:25:04 +0800

    函数名改为gui_verify,添加注释

--- verify.py
+++ verify.py
@@ -1,10 +1,12 @@
+import tkinter as tk
 import pyotp
 import qrcode
-import tkinter as tk
+from tkinter import messagebox
 from PIL import ImageTk
 
 
 def verify(user):
+    """读取秘钥文件"""
     try:
         with open('secdata.pass', 'r') as f:
             sec = f.read()
@@ -16,6 +18,7 @@ def verify(user):
 
 
 def init():
+    """生成秘钥和对应二维码并返回Tk图像"""
     sec = pyotp.random_base32()
     with open('secdata.pass', 'w') as f:
         f.write(sec)
@@ -27,10 +30,11 @@ def init():
     return tk_img
 
 
-def verify_gui():
+def gui_verify():
+    """生成验证窗口"""
     root = tk.Tk()
     root.geometry("300x100")
-    root.protocol("WM_DELETE_WINDOW", exit)
+    root.protocol("WM_DELETE_WINDOW", exit)  # 监听窗口关闭事件，阻止运行下一步
 
     f1 = tk.Frame(root, pady=25)
     f1.pack()
@@ -40,11 +44,14 @@ def verify_gui():
 
     e1 = tk.Entry(f1)
     e1.pack()
-    b1 = tk.Button(root, text='验证', command=lambda: gui_exit(e1.get(), root))
+    b1 = tk.Button(root, text='验证', command=lambda: gui_exit(e1.get(), root))  # 当按钮被按下时，使用匿名函数调用检测函数
     b1.pack()
     root.mainloop()
 
 
 def gui_exit(user, window):
+    """检测验证是否成功"""
     if verify(user):
         window.destroy()
+    else:
+        messagebox.showerror("error", "验证码错误")
